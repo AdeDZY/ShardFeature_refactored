4c78efecd4fc3561119c56a2d02370b735910c8c
zhuyund <zhuyund@cs.cmu.edu>
2017-06-07 22:34:08 -0400

    add bigram

--- shardFeature.cpp
+++ shardFeature.cpp
@@ -52,6 +52,48 @@ void readQueriesToTerms(unordered_set<string> &queryTerms, const char *queryFile
     }
 }
 
+
+void readQueryBigrams(set<pair<int, int> > &queryBigrams, const char *queryFile, indri::index::Index * index, indri::collection::Repository & repo){
+    queryBigrams.clear();
+    ifstream queryStream;
+    queryStream.open(queryFile);
+
+    int termID;
+    vector<int> termIds;
+    string line;
+    while(! queryStream.eof()) {
+        termIds.clear();
+        getline(queryStream, line);
+        if(line.empty()) {
+            if(! queryStream.eof()) {
+                cout << "Error: Empty line found in query file." <<endl;
+                exit(-1);
+            }
+            else {
+                break;
+            }
+        }
+
+        string term;
+        stringstream ss;
+        ss.str(line);
+        while(!ss.eof()){
+            ss>>term;
+            string stem = repo.processTerm(term);
+            termID = index->term(stem);
+            termIds.push_back(termID);
+        }
+
+        for(int i = 0; i < termIds.size() - 1; i++){
+            if(termIds[i] >= 0 && termIds[i + 1] >= 0){
+                queryBigrams.insert(std::make_pair<int, int>(termIds[i], termIds[i + 1])) ;
+                cout<<termIds[i]<<"_"<<termIds[i + 1]<<endl;
+            }
+        }
+    }
+    queryStream.close();
+}
+
 void mapQueryTermsToId(unordered_set<string> &queryTerms,
                        unordered_set<int> &queryTermIds,
                        unordered_map<int, string> &id2stem,
@@ -97,7 +139,8 @@ void get_document_vector(indri::index::Index *index,
                          const int docid,
                          const unordered_set<int> &queryTerms,
                          const unordered_map<int, string> &id2stem,
-                         unordered_map<string, FeatVec> featureList[]) {
+                         unordered_map<string, FeatVec> featureList[],
+                         map<pair<string, string>, FeatVec> &bigramFeatures) {
 
     int nFields = 4;
     string fields[4] = {"body", "title", "url", "inlink"};
@@ -174,6 +217,56 @@ void get_document_vector(indri::index::Index *index,
         featureList[fdx][" "].updateFeature(docLens[fdx], docLens[fdx]);
     }
 
+    // bigram features
+    // the whole documents
+    int docLen = 0;
+    map<pair<int, int>, int> docVec;
+    docVec.clear();
+    for (int t = 0; t < terms.size() - 1; t++)
+    {
+
+        docLen++;
+
+        if (terms[t] <= 0) // [OOV]
+            continue;
+        if (terms[t + 1] <= 0)
+            continue;
+
+        std::pair<int, int> p = std::make_pair(terms[t], terms[t + 1]);
+        if (queryTerms.find(p) == queryTerms.end())  // not query term
+            continue;
+
+        if (docVec.find(p) != docVec.end()) {
+            docVec[p]++;
+        }
+        else {
+            docVec[p] = 1;
+        }
+
+    }
+
+    if(docLen > 0){
+        // update feature
+        map<pair<string, string>, int>::iterator it;
+        int freq;
+        pair<int, int> bigram;
+        for(it = docVec.begin(); it != docVec.end(); it++){
+            bigram = it->first;
+            freq = it->second;
+            int termID_0 = bigram->first;
+            int termID_1 = bigram->second;
+            string stem_0 = (id2stem.find(termID_0))->second;
+            string stem_1 = (id2stem.find(termID_1))->second;
+            bigram_stem = std::make_pair(stem_0, stem_1);
+            if(features.find(bigram) == features.end())
+                bigramFeature[bigram_stem] = FeatVec(1, freq/double(docLen), freq);
+            else
+                bigramFeatures[bigram_stem].updateFeature(freq, docLen);
+        }
+    }
+
+    // to get shard size and total term freq in shards
+    bigramFeatures[std::make_pair(" ", " ")].updateFeature(docLen, docLen);
 
     // Finish processing this doc
 
@@ -206,6 +299,20 @@ void writeFeatures(const unordered_map<string, FeatVec> featureList[],
     outStream.close();
 }
 
+void writeBigramFeatures(const map<pair<string, string>, FeatVec> &features,
+                         const string outFile){
+
+    ofstream outStream;
+    outStream.open(outFile.c_str());
+
+    map<pair<string, string>, FeatVec>::const_iterator it;
+    for(it = features.begin(); it != features.end(); it++){
+        outStream<<(it->first).first<<"_"<<(it->first).second;
+        outStream<<" ";
+        outStream<<it->second.df<<" "<<it->second.sum_tf<<" "<<it->second.sum_prob<<endl;
+    }
+    outStream.close();
+}
 
 
 int main(int argc, char **argv){
@@ -217,8 +324,9 @@ int main(int argc, char **argv){
         cout<<repoPaths[i]<<endl;
     }
     string extidFile = argv[nRepos + 2];    // doc extid in one shard, each line is an extid
-    string outFile = argv[nRepos + 3];      // output file
-    std::string queryFile = argv[nRepos + 4]; // each line is a query, seperated by space
+    string outUnigramFile = argv[nRepos + 3];      // output file
+    string outBigramFile = argv[nRepos + 4];      // output file
+    std::string queryFile = argv[nRepos + 5]; // each line is a query, seperated by space
 
     ifstream extidStream;
     extidStream.open(extidFile.c_str());
@@ -255,7 +363,8 @@ int main(int argc, char **argv){
     for(int i = 0; i < nFields; i++){
         featureList[i][" "] = FeatVec();
     }
-
+    map<pair<string, string>, FeatVec> bigramFeatures;
+    bigramFeatures[std::make_pair(" ", " ")] = FeatVec();
 
     vector <string> extids;
     vector <int> intids;
@@ -279,7 +388,7 @@ int main(int argc, char **argv){
         }
         if (intid > 0){
             cout<<extid<<" "<<repoPaths[i]<<endl;
-            get_document_vector(indexes[i], intid, list_queryTermIDs[i], list_id2stems[i], featureList);
+            get_document_vector(indexes[i], intid, list_queryTermIDs[i], list_id2stems[i], featureList, bigramFeatures);
         }
 
     }
@@ -289,7 +398,8 @@ int main(int argc, char **argv){
         repos[i].close();
     }
 
-    writeFeatures(featureList, outFile);
+    writeFeatures(featureList, outUnigramFile);
+    writeBigramFeatures(bigramFeatures, outBigramFile);
 
     return 0;
 }
