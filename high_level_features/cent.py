3281c515be1a7c20ad1e19847859d1088d0340db
AdeDZY <zhuyund@cs.cmu.edu>
2017-06-08 23:22:47 -0400

    remove reverse and bin rank

--- high_level_features/cent.py
+++ high_level_features/cent.py
@@ -69,15 +69,15 @@ def gen_lst(field_stats, query, method, miu):
             res[shard] = " ".join([str(s) for s in ss])
 
     # get lm_inverse_rank, lm_binned_rank
-    if method == "lm":
-        tmp = [(score, shard) for shard, score in res.items()]
-        tmp = sorted(tmp, reverse=True)
-        for i in range(len(tmp)):
-            score, shard = tmp[i]
-            r = float(i + 1)
-            reversed_rank = 1 / r
-            binned_rank = int(r / 10)
-            res[shard] = ('{0} {1} {2}'.format(s, reversed_rank, binned_rank))
+    #if method == "lm":
+    #    tmp = [(score, shard) for shard, score in res.items()]
+    #    tmp = sorted(tmp, reverse=True)
+    #    for i in range(len(tmp)):
+    #        score, shard = tmp[i]
+    #        r = i + 1
+    #        reversed_rank = 1.0 / r
+    #        binned_rank = r / 10
+    #        res[shard] = ('{0} {1} {2}'.format(s, reversed_rank, binned_rank))
 
     return res
 
